2410e5e35193cfa865d062c625317e6166c19fe5
Colin <colin.diesh@gmail.com>
2021-02-10 15:50:23 -0700

    Add version number

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Plugin from '@jbrowse/core/Plugin'
+import packagejson from '../package.json'
 export default class MyProjectPlugin extends Plugin {
   name = 'MyProject'
+  version = packagejson.version
 }
