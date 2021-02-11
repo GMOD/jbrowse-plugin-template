7a1cf24a098300ec3371c6334b81ea0a04138e03
Colin <colin.diesh@gmail.com>
2021-02-10 17:09:35 -0700

    Select version specifically

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Plugin from '@jbrowse/core/Plugin'
-import packagejson from '../package.json'
+import { version } from '../package.json'
 export default class MyProjectPlugin extends Plugin {
   name = 'MyProject'
-  version = packagejson.version
+  version = version
 }
