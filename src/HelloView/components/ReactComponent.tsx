5e58b531b475ff16998c47f6dd7592f6522896bf
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-08-31 14:38:22 -0600

    Spelling fix

--- src/HelloView/components/ReactComponent.tsx
+++ src/HelloView/components/ReactComponent.tsx
@@ -7,7 +7,7 @@ export default function ReactComponent() {
       <h1>Hello plugin developers!</h1>
       <button
         onClick={() => {
-          setPushed('Woah! You pushed the button!')
+          setPushed('Whoa! You pushed the button!')
         }}
       >
         Push the button
