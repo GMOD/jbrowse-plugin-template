fef752c5016c537459eb89daefc14648f8198625
Colin <colin.diesh@gmail.com>
2023-03-17 14:03:42 -0600

    Add elementid for view

--- src/HelloView/stateModel.ts
+++ src/HelloView/stateModel.ts
@@ -1,7 +1,11 @@
+import { ElementId } from '@jbrowse/core/util/types/mst'
 import { types } from 'mobx-state-tree'
 
 const stateModel = types
-  .model({ type: types.literal('HelloView') })
+  .model({
+    id: ElementId,
+    type: types.literal('HelloView'),
+  })
   .actions(() => ({
     // unused but required by your view
     setWidth() {},
