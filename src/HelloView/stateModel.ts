f4b660cb6ce3c28367f70f45278c34d14619a0c3
Colin <colin.diesh@gmail.com>
2025-02-04 12:52:55 -0500

    Add menuItems function to HelloView

--- src/HelloView/stateModel.ts
+++ src/HelloView/stateModel.ts
@@ -1,3 +1,4 @@
+import { MenuItem } from '@jbrowse/core/ui'
 import { ElementId } from '@jbrowse/core/util/types/mst'
 import { types } from 'mobx-state-tree'
 
@@ -7,8 +8,15 @@ const stateModel = types
     type: types.literal('HelloView'),
   })
   .actions(() => ({
-    // unused but required by your view
+    // unused by this view but it is updated with th current width in pixels of
+    // the view panel
     setWidth() {},
+  }))
+  .views(() => ({
+    // unused by this view, but represents of 'view level' menu items
+    menuItems(): MenuItem[] {
+      return []
+    },
   }))
 
 export default stateModel
